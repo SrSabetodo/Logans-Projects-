2b2e3871c48f7a89be287a353c214c245d9931f7
Logan Kreun <kreun019@umn.edu>
2021-05-13 22:53:28 -0500

    Big gains

--- C++/Project 3/SentenceSample/SentenceSample.cpp
+++ C++/Project 3/SentenceSample/SentenceSample.cpp
@@ -1,26 +1,45 @@
-#include "SentenceSample.h"
-#include <iostream>
-#include <string>
-#include <cctype>
-#include <vector>
-#include <sstream>
+/**
+ * @author Logan Kreun 
+ * @details Class to split sentences into samples of size sentence sample 
+ */
 
+#include "SentenceSample.h"
+
+/**
+ * Default constructor for constructing SentenceSample objects
+ * @param sentenceString string that makes up the string portion of the sample 
+ * @param nextWord the 'next word' after sentenceString 
+ */
 SentenceSample::SentenceSample(std::string sentenceString, std::string nextWord)
 {
     this->sentenceString = sentenceString;
     this->nextWord = nextWord;
 }
 
+/**
+ * Getter for sentenceString
+ * @return string sentenceString 
+ */
 std::string SentenceSample::getSentence()
 {
     return sentenceString;
 }
 
+/**
+ * Getting for next word in sample 
+ * @return nextWord part of sentence sample 
+ */
 std::string SentenceSample::getNextWord()
 {
     return nextWord;
 }
 
+/**
+ * Overload << operator to output sentence samples in a readable manner 
+ * @param os output stream 
+ * @param sample SentenceSample sample to send to output stream
+ * @return output stream w/ newly created SentenceSample string 
+ */
 std::ostream &operator<<(std::ostream &os, SentenceSample &sample)
 {
     os << "\"" << sample.sentenceString << "\""
@@ -28,6 +47,11 @@ std::ostream &operator<<(std::ostream &os, SentenceSample &sample)
     return os;
 }
 
+/**
+ * Finds the number of words in a sample, which is equal to the number of spaces + 1 
+ * @param input string to find number of words 
+ * @return number of words in string 
+ */ 
 int SentenceSample::getNumWords(std::string input)
 {
     int numSpaces = 0;
@@ -41,17 +65,101 @@ int SentenceSample::getNumWords(std::string input)
     return (numSpaces + 1);
 }
 
+/**
+ * returnWordsInString accepts a string an number of words, and returns an array of size length of the words in input 
+ * @param input string to 'split' into words 
+ * @param length number of words in string 
+ * @param stringArray array consisting of the words in input  
+ */
 std::string *SentenceSample::returnWordsInString(std::string input, int length)
 {
-    std::istringstream ss(input);
-    int iterator = 0;
-    std::string *stringArray = new std::string[length];
+    std::istringstream ss(input);                       // create an istringsstream object with our input string
+    int iterator = 0;                                   // start at 0
+    std::string *stringArray = new std::string[length]; // create an array of size length
     for (int i = 0; i < length; i++)
     {
         std::string word;
-        ss >> word;
-        stringArray[iterator] = word;
+        ss >> word;                   // extract words from ss, which was constructed using input
+        stringArray[iterator] = word; // add words to stringArray
         iterator++;
     }
     return stringArray;
+}
+
+/**
+ * toSamples accepts a string and segment size, and splits the string into segments according to segmentSize, and returns them in a vector 
+ * @param inputString string to split into samples 
+ * @param segmentSize size of segments to split inputString into 
+ * @return vector of SentenceSample objects, representing the samples the string has been split into 
+ */
+std::vector<SentenceSample> SentenceSample::toSamples(std::string inputString, int segmentSize)
+{
+    std::string newstring = ""; // init to empty
+
+    for (int i = 0; i < inputString.length(); i++) // process string, get rid of uppercase and non alphabetic symbols
+    {
+        if (isupper(inputString[i]))
+        {
+            inputString[i] = tolower(inputString[i]);
+        }
+
+        if (isspace(inputString[i]) || isalpha(inputString[i]))
+        {
+            newstring += inputString[i];
+        }
+    }
+
+    newstring += SentenceSample::STOP; // add STOP character at the end
+
+    int numberWords = SentenceSample::getNumWords(newstring);                               // get num of words
+    std::vector<SentenceSample> wordArray(numberWords);                                     // create vector wordArray of size numberWords
+    std::string *stringArray = SentenceSample::returnWordsInString(newstring, numberWords); // get an array of all the words in inputString
+    int start = 0, stop;
+
+    for (int n = 0; n < numberWords; n++) // n represents segment size, that will keep increasing
+    {
+        std::string sentence = ""; // start with an empty sentence
+        if (n < segmentSize)       // first few cases where n is less than segmentSize, so we just keep adding more words to sentence
+        {
+            // puts("here");
+            for (int i = 0; i < n; i++)
+            {
+                if (n > 1) // 'default' case where we just add empty string as the next word
+                {
+                    sentence += stringArray[i] + " ";
+                }
+                else // Special first case when we just want to add the word and no space after it
+                {
+                    sentence += stringArray[i];
+                }
+            }
+
+            if (isspace(sentence[sentence.length() - 1])) // clean up so there are no extra spaces after a sample
+            {
+                sentence = sentence.substr(0, sentence.length() - 1);
+            }
+
+            wordArray[n] = SentenceSample(sentence, stringArray[n]); // add sample to wordArray
+        }
+        else // all other cases when n > segment size
+        {
+            stop = n;
+            for (int i = start; i < stop; i++) // by incrementing using start and stop, we can 'step' through the inputString 1 word at a time
+            {
+                if (i < (stop - 1))
+                {
+                    sentence += stringArray[i] + " ";
+                }
+                else
+                {
+                    sentence += stringArray[i];
+                }
+            }
+            wordArray[n] = SentenceSample(sentence, stringArray[n]); // add new object to our array
+            start++;
+            stop++;
+        }
+    }
+    delete[] stringArray; // cleanup
+    return wordArray;
 }
--- C++/Project 3/SentenceSample/main.cpp
+++ C++/Project 3/SentenceSample/main.cpp
@@ -1,51 +1,58 @@
+/**
+ * @author Logan Kreun
+ * @details Simple little file to test out SentenceSample 
+ */
 #include "SentenceSample.h"
-#include <iostream>
-#include <string>
-#include <cctype>
-#include <vector>
 
 int main()
 {
 
-std::cout << SentenceSample::STOP<<std::endl;
+    std::cout << SentenceSample::STOP << std::endl;
 
-SentenceSample ls("Hello my name is logan","alpha");
-std::cout<<ls<<std::endl;
+    SentenceSample ls("Hello my name is logan", "alpha");
+    std::cout << ls << std::endl;
 
-std::vector<SentenceSample> vect;
-vect = SentenceSample::toSamples("Hello my name is logan and I like bread.",3);
-for(int i = 0; i < vect.size(); i++)
+    std::vector<SentenceSample> vect;
+    vect = SentenceSample::toSamples("Hello my name is logan", 3);
+    for (int i = 0; i < vect.size(); i++)
     {
-        std::cout << vect[i] <<", ";
+        std::cout << vect[i] << ", ";
     }
-vect.clear();
+    vect.clear();
 
-std::cout << std::endl;
+    std::cout << std::endl;
 
-vect = SentenceSample::toSamples("The reason that I feel this say.",2);
-for(int i = 0; i < vect.size(); i++)
+    vect = SentenceSample::toSamples("The reason that I feel this say.", 2);
+    for (int i = 0; i < vect.size(); i++)
     {
-        std::cout << vect[i] <<", ";
+        std::cout << vect[i] << ", ";
     }
-vect.clear();
+    vect.clear();
 
-std::cout << std::endl;
+    std::cout << std::endl;
 
-vect = SentenceSample::toSamples("Hello my name is logan and I like bread",0);
-for(int i = 0; i < vect.size(); i++)
+    vect = SentenceSample::toSamples("Hello my name is logan and I like bread", 0);
+    for (int i = 0; i < vect.size(); i++)
     {
-        std::cout << vect[i] <<", ";
+        std::cout << vect[i] << ", ";
     }
-vect.clear();
+    vect.clear();
 
-std::cout << std::endl;
+    std::cout << std::endl;
 
-
-vect = SentenceSample::toSamples("@HeLlO mY! Name! is! Logian! and I don't know what to say", 4);
-for(int i = 0; i < vect.size(); i++)
+    vect = SentenceSample::toSamples("@HeLlO mY! Name! is! Logian!", 4);
+    for (int i = 0; i < vect.size(); i++)
     {
-        std::cout << vect[i] <<", ";
+        std::cout << vect[i] << ", ";
     }
-vect.clear();
-return 0;
+    vect.clear();
+    return 0;
 }
+
+// .
+// "Hello my name is logan" -> alpha
+
+// "" -> hello, "hello" -> my, "hello my " -> name, "hello my name" -> is, "my name is" -> logan, "name is logan" -> and, "is logan and" -> i, "logan and i" -> like, "and i like" -> bread., 
+// "" -> the, "the" -> reason, "the reason" -> that, "reason that" -> i, "that i" -> feel, "i feel" -> this, "feel this" -> say., 
+// "" -> hello, "" -> my, "" -> name, "" -> is, "" -> logan, "" -> and, "" -> i, "" -> like, "" -> bread., 
+// "" -> hello, "hello" -> my, "hello my " -> name, "hello my name " -> is, "hello my name is" -> logian, "my name is logian" -> and, "name is logian and" -> i, "is logian and i" -> dont, "logian and i dont" -> know, "and i dont know" -> what, "i dont know what" -> to, "dont know what to" -> say.,
